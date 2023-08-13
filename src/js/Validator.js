a3bbd1b180c2c94939be60b02ab9bbd187a57a84
Yuriy Starostin <yurik_05@inbox.ru>
2023-08-13 14:59:03 +0400

    feat: solution fix

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -2,7 +2,12 @@
 
 export default class Validator {
   static validateUsername(username) {
-    const regex = /^(?!.*[-_]{2,})(?!.*\d{4,})[a-zA-Z0-9-_][a-zA-Z0-9-_]*[a-zA-Z0-9-_]$/;
-    return regex.test(username);
+    const startEndRegex = /^[a-zA-Z0-9-_].*[a-zA-Z0-9-_]$/;
+    const limitationRegex = /^(?!.*\d{4,})[^-_0-9]\w*[^-_0-9]$/;
+
+    return (
+      startEndRegex.test(username)
+      && limitationRegex.test(username)
+    );
   }
 }
--- src/js/__tests__/Validator.test.js
+++ src/js/__tests__/Validator.test.js
@@ -4,6 +4,7 @@ import Validator from '../Validator';
 
 test('should validate username', () => {
   expect(Validator.validateUsername('john_doe')).toBe(true);
-  expect(Validator.validateUsername('alice-123')).toBe(true);
-  expect(Validator.validateUsername('user_1')).toBe(true);
+  expect(Validator.validateUsername('user1')).toBe(false);
+  expect(Validator.validateUsername('2user')).toBe(false);
+  expect(Validator.validateUsername('us-_er')).toBe(false);
 });
